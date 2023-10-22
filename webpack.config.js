a04932f771c9afd155f693ee02228dce78fde00f
comradexlight <comradexlight@gmail.com>
2023-10-22 15:13:13 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  target: 'web',
+  devtool: 'inline-source-map',
+  optimization: {
+    removeEmptyChunks: true,
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    port: 9000,
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader, options: { esModule: true } }, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
+
